ed9a656474cbbcefdae346831055aba0b7e3b3c7
Felipe Macci <felipemacci@gmail.com>
2023-05-01 16:00:40 -0300

    refactoring csv parser

--- src/utils/csv-parser.js
+++ src/utils/csv-parser.js
@@ -4,16 +4,14 @@ import fs from 'fs'
 
 (async () => {
   const csvPath = new URL('../tmp/temp.csv', import.meta.url)
-  const file = fs.createReadStream(csvPath).pipe(parse())
-
-  let isFirstRow = true
+  const file = fs.createReadStream(csvPath)
+    .pipe(parse({
+      delimiter: ',',
+      skipEmptyLines: true,
+      fromLine: 2
+    }))
 
   for await (const chunk of file) {
-    if (isFirstRow) {
-      isFirstRow = false
-      continue
-    }
-
     axios.post('http://localhost:3333/tasks', {
       title: String(chunk[0]),
       description: String(chunk[1])
